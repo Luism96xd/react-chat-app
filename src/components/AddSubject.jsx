ee444f3da7385500cbc745b4170219a352ac160d
Luis Rivas <chromxd0@gmail.com>
2023-02-16 21:46:42 -0400

    Replacing firebase with postgres database

--- src/components/AddSubject.jsx
+++ src/components/AddSubject.jsx
@@ -2,11 +2,33 @@ import {addDoc, updateDoc, collection, doc, getDoc} from 'firebase/firestore';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { db } from '../firebase';
+import axios from 'axios';
 
 export const AddSubject = ({id, setIsOpen}) => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [subject, setSubject] = useState([]);
 
+    useEffect(() => {
+        const getData = async () =>{
+            setLoading(true);
+            if (id == null){
+                return;
+            }
+            try {
+                console.log("Fetching data");
+                const response = await axios.get('https://virtual-assistant.onrender.com/subjects/'+id);
+                setSubject(response.data[0]);
+            }catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        }
+        return () => {
+            getData();
+        }
+        
+    }, [id]);
+    /*
     useEffect(() => {
         const getData = async () =>{
             if (id == null){
@@ -32,36 +54,47 @@ export const AddSubject = ({id, setIsOpen}) => {
         getData();
       }
     }, [id])
-    
+    */
 
-  const handleCreate = async (e) =>{
-    e.preventDefault();
-    const name = e.target[0].value;
-    const description = e.target[1].value;
-    try{
-        const res = await addDoc(collection(db, "topicos"), { 
-            nombre: name,
-            descripcion: description
-        });
-        await updateDoc(doc(db, "topicos", res.id), { 
-            id: res.id,
-        });
-        setIsOpen(false)
-    }catch(error){
-        console.log(error);
+    const handleCreate = async (e) =>{
+        e.preventDefault();
+        const name = e.target[0].value;
+        const description = e.target[1].value;
+
+        try {
+            await axios.post(
+                'https://virtual-assistant.onrender.com/subjects/', {name, description}
+            );
+        }catch (error) {
+            console.error(error.message);
+        }
+
+        /*
+        try{
+            const res = await addDoc(collection(db, "topicos"), { 
+                nombre: name,
+                descripcion: description
+            });
+            await updateDoc(doc(db, "topicos", res.id), { 
+                id: res.id,
+            });
+            setIsOpen(false)
+        }catch(error){
+            console.log(error);
+        }
+        */
+
     }
-    
-  }
-  return (
+    return (
     <div>
         <form onSubmit={handleCreate} action="POST" className='form'>
                 <div className='form-inline'>
                     <label htmlFor="name">Nombre</label>
-                    <input type="text" id="name" defaultValue={title} required/>
+                    <input type="text" id="name" defaultValue={subject.name} required/>
                 </div>
                 <div className='form-inline'>
                     <label htmlFor="description">Descripción</label>
-                    <textarea name="description" id="description" cols="30" rows="10" defaultValue={content}></textarea>
+                    <textarea name="description" id="description" cols="30" rows="10" defaultValue={subject.description}></textarea>
                 </div>
                 
                 <div className='buttons'>
@@ -74,5 +107,5 @@ export const AddSubject = ({id, setIsOpen}) => {
                 </div>
         </form>
     </div>
-  )
+    )
 }
--- src/components/Subjects.jsx
+++ src/components/Subjects.jsx
@@ -1,6 +1,7 @@
 import Edit from '../img/edit.svg';
 import Delete from '../img/delete.svg';
 import React, { useContext, useState, useEffect} from "react";
+import axios from 'axios';
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../firebase";
 import { SubjectContext } from "../context/SubjectContext";
@@ -9,20 +10,33 @@ import { AddSubject } from "../components/AddSubject";
 
 
 const Subjects = () =>{
-    const [subjects, setSubjects] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
     const {data, dispatch} = useContext(SubjectContext);
+    const [loading, setLoading] = useState(true);
+    const [subjects, setSubjects] = useState([]);
   
     useEffect(() => {
         const items = [];
         const getSubjects = async () =>{
+            setLoading(true);
+            try {
+                const response = await axios.get('https://virtual-assistant.onrender.com/subjects/');
+                setSubjects(response.data);
+                console.log(response.data)
+              } catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+            
+            /*
             console.log("Fetching Subjects from database");
             const q = query(collection(db, "topicos"));
             const querySnapshot = await getDocs(q);
             querySnapshot.forEach((doc) => {
                 items.push(doc.data());
             });
-            setSubjects(items);
+            */
+            //setSubjects(items);
         }
         return () => {
             getSubjects();
@@ -41,21 +55,23 @@ const Subjects = () =>{
     return (
         <div className="sidebar">
             <ul className="list">
-                {
-                Object.entries(subjects)?.sort((a, b) => b[1].createdAt - a[1].createdAt)
-                    .map((subject) => {
-                return (
-                    <li className="list-item" 
-                        key={subject[0]} 
-                        onClick={() => handleSelect(subject[1])}>
-                        <span>{subject[1].nombre}</span>
-                        <div className="icons">
-                            <img className="icon" src={Edit} alt="edit" onClick={() => setIsOpen(true)}/>
-                            <img className="icon" src={Delete} alt="delete" onClick={() => setIsOpen(true)}/>
-                        </div>
-                    </li>
-                )
-                })
+                {loading && <div>Loading...</div>}
+                {!loading && (
+                    Object.entries(subjects)?.sort((a, b) => b[1].createdAt - a[1].createdAt)
+                        .map((subject) => {
+                    return (
+                        <li className="list-item" 
+                            key={subject[1].id_subject} 
+                            onClick={() => handleSelect(subject[1])}>
+                            <span>{subject[1].name}</span>
+                            <div className="icons">
+                                <img className="icon" src={Edit} alt="edit" onClick={() => setIsOpen(true)}/>
+                                <img className="icon" src={Delete} alt="delete" onClick={() => setIsOpen(true)}/>
+                            </div>
+                        </li>
+                        )
+                    })
+                    )
                 }    
             
             </ul>
@@ -63,8 +79,8 @@ const Subjects = () =>{
                 <button className="btn btn-default" onClick={handleCreate}>Agregar Nuevo</button>
             </div>
             {isOpen && 
-                <Modal title={(data.subject.id)? data.nombre : "Agregar nuevo Tópico"}>
-                    <AddSubject id={data.subject.id} setIsOpen={setIsOpen}/>
+                <Modal title={(data.subject.id_subject)? data.name : "Agregar nuevo Tópico"}>
+                    <AddSubject id={data.subject.id_subject} setIsOpen={setIsOpen}/>
                 </Modal>
             }
         </div>
